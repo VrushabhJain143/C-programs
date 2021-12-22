4183ee481b735e4b192d842c1ef1c30984d26db9
VrushabhJain143 <81697379+VrushabhJain143@users.noreply.github.com>
2021-12-22 18:01:36 +0530

    Add files via upload

---
+++ Accept two values.c
@@ -0,0 +1,14 @@
+// program to accept value of two number and print their addition 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter the value");
+	scanf("%d",&a);
+	printf("Enter the value");
+	scanf("%d",&b);
+	c=a+b;
+	printf(" The additions is %d",c);
+	getch();
+}
